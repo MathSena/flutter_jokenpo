f78e90880d7e2a18bae14ab952b9ebf2e8d23c11
Matheus Sena <MathSena07@hotmail.com>
2022-10-03 21:13:35 -0300

    Finalizando APP

--- lib/Jogo.dart
+++ lib/Jogo.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 class Jogo extends StatefulWidget {
@@ -8,6 +9,59 @@ class Jogo extends StatefulWidget {
 }
 
 class _JogoState extends State<Jogo> {
+
+  var _imagemApp = AssetImage("imagens/padrao.png");
+  var _mensagem = "Escolha uma opção abaixo";
+
+  void _opcaoSelecionada(String escolharUsuario){
+
+    var opcoes =  ["pedra", "papel", "tesoura"];
+    var numero = Random().nextInt(3);
+    var escolhaApp = opcoes[numero];
+
+    switch(escolhaApp){
+      case 'pedra':
+        setState(() {
+          this._imagemApp = AssetImage("imagens/pedra.png");
+        });
+        break;
+
+      case 'papel':
+        setState(() {
+          this._imagemApp = AssetImage("imagens/papel.png");
+        });
+        break;
+
+      case 'tesoura':
+        setState(() {
+          this._imagemApp = AssetImage("imagens/tesoura.png");
+        });
+
+        break;
+    }
+
+    if((escolharUsuario == "pedra" && escolhaApp == "tesoura") ||
+        (escolharUsuario == "tesoura" && escolhaApp == "papel") ||
+        (escolharUsuario == "papel" && escolhaApp == "pedra")
+    ){
+      setState(() {
+        this._mensagem = "Você venceu!";
+      });
+    }else if(
+        (escolhaApp == "pedra" && escolharUsuario == "tesoura") ||
+        (escolhaApp == "tesoura" && escolharUsuario == "papel") ||
+        (escolhaApp == "papel" && escolharUsuario == "pedra")
+    ){
+      setState(() {
+        this._mensagem = "Você perdeu!";
+      });
+    }else{
+      setState(() {
+        this._mensagem = "Empate!";
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,10 +71,6 @@ class _JogoState extends State<Jogo> {
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          //text
-          //imagem
-          //text resultado
-          //linha 3 imagens
           Padding(
               padding: EdgeInsets.only(top:32, bottom: 16),
             child: Text(
@@ -33,11 +83,11 @@ class _JogoState extends State<Jogo> {
             ),
 
           ),
-          Image.asset("imagens/padrao.png"),
+          Image(image: this._imagemApp),
           Padding(
             padding: EdgeInsets.only(top:32, bottom: 16),
             child: Text(
-              "Escolha uma opção abaixo",
+              this._mensagem,
               textAlign: TextAlign.center,
               style: TextStyle(
                   fontSize: 20,
@@ -50,9 +100,24 @@ class _JogoState extends State<Jogo> {
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
+              GestureDetector(
+                onTap: ()=> _opcaoSelecionada("pedra"),
+                child: Image.asset("imagens/pedra.png", height: 95),
+              ),
+              GestureDetector(
+                onTap: ()=> _opcaoSelecionada("papel"),
+                child: Image.asset("imagens/papel.png", height: 95),
+              ),
+              GestureDetector(
+                onTap: ()=> _opcaoSelecionada("tesoura"),
+                child: Image.asset("imagens/tesoura.png", height: 95),
+              ),
+
+              /*
               Image.asset("imagens/pedra.png", height: 95),
               Image.asset("imagens/papel.png", height: 95),
               Image.asset("imagens/tesoura.png", height: 95),
+               */
             ],
           )
         ],
